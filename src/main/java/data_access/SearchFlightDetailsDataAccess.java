04c009397123e753cc34573b65040387f77ebd65
Javris-T2 <javris-t2@Javris-T2deMacBook-Air.local>
2025-11-24 18:07:39 -0500

    train of thoughts and ideas on fixing

--- src/main/java/data_access/SearchFlightDetailsDataAccess.java
+++ src/main/java/data_access/SearchFlightDetailsDataAccess.java
@@ -4,4 +4,8 @@ import data_access.ViewActiveFlightDataAccess;
 
 public class SearchFlightDetailsDataAccess {
     //TODO: use Mcqueen's data access to resolve flights to flight, then return.
+    //Either use McQ's data interface to get the flights, then sort out the flight needed here
+    //then, used by self usecase to retreive fight numebr by the returned flight here.
+    //or use interactor directly get all flights then retrieve the flight--- ie. this data access
+    //file is not required in second method.
 }
