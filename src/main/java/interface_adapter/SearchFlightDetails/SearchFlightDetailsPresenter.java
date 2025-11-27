593dc562de1f82f103f7dd60a9378ce06ab8b8aa
Smouthiee <frankjiasheng.li@mail.utoronto.ca>
2025-11-27 16:44:12 -0500

    Changed variables for use case 1, as OpenSky API does not provide previous usecase variables.

--- src/main/java/interface_adapter/SearchFlightDetails/SearchFlightDetailsPresenter.java
+++ src/main/java/interface_adapter/SearchFlightDetails/SearchFlightDetailsPresenter.java
@@ -2,18 +2,23 @@ package interface_adapter.SearchFlightDetails;
 
 import use_case.SearchFlightDetails.SearchFlightDetailsOutputBoundary;
 import use_case.SearchFlightDetails.SearchFlightDetailsOutputData;
-import view.SearchFlightDetailsScreen;
 
 public class SearchFlightDetailsPresenter implements SearchFlightDetailsOutputBoundary {
 
-    private final SearchFlightDetailsScreen view;
+    private final SearchFlightDetailsViewModel viewModel;
 
-    public SearchFlightDetailsPresenter(SearchFlightDetailsScreen view) {
-        this.view = view;
+    public SearchFlightDetailsPresenter(SearchFlightDetailsViewModel viewModel) {
+        this.viewModel = viewModel;
     }
 
     @Override
-    public void present(SearchFlightDetailsOutputData outputData) {
-        view.showFlightDetails(outputData);
+    public void present(SearchFlightDetailsOutputData data) {
+        String text = "Callsign: " + data.getCallSign() + "\n" +
+                "Origin Country: " + data.getOriginCountry() + "\n" +
+                "Time Position: " + data.getTimePosition() + "\n" +
+                "Squawk: " + data.getSquawk() + "\n" +
+                "On Ground: " + data.isOnGround() + "\n";
+
+        viewModel.setDisplayText(text);
     }
 }
